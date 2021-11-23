63ec760c5800b03124b8a0cbf8bd1a8a50ff351d
gustavoPaulo <gustavo-tj@hotmail.com>
2021-11-23 17:14:47 -0300

    Adicionando validation no pom

--- src/main/java/com/g/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/g/cursomc/dto/CategoriaDTO.java
@@ -1,11 +1,15 @@
 package com.g.cursomc.dto;
 
 import com.g.cursomc.domain.Categoria;
+
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
 public class CategoriaDTO implements Serializable {
 
     private Integer id;
+
+    @NotEmpty
     private String nome;
 
     public CategoriaDTO() {
